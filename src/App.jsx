7258a8c75c578761c88e7f8bc6782dc7094a7945
anpan7088 <anpan7088@gmail.com>
2024-06-21 17:26:45 +0200

    Bootstrap, sass and some node utisl added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from 'react-bootstrap'
+
+// Bootstrap Bundle JS, bootstrap.css is imported in the App.scss
+import "./scss/App.scss";
+import "./scss/custom.scss"
+// #mkd java script za bootstrap ne sum siguren bash dali treba oti upotrebuime komponenti react-bootstrap
+// ali neka sto via red sea za sega
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
