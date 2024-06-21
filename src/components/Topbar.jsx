b7628e5a09dccae2038e05c477e325826dcfa531
anpan7088 <anpan7088@gmail.com>
2024-06-21 17:52:06 +0200

    Update topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -10,43 +10,43 @@ const Topbar = () => {
     const [showLogoutPopUp, setShowLogoutPopUp] = useState(false);
 
     return (
-    <>
-        <Navbar bg="primary" variant="dark" expand="md">
-            <Container>
-                <Navbar.Brand href="/home">Dorm Reviews</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <NavDropdown title="Dorms" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/dorms">Dorm Management</NavDropdown.Item>
-                            <NavDropdown.Item href="/reviews">Dorm reviews</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Nav className="ml-auto">
-                        <NavDropdown title={userName || "Login"} id="user-dropdown">
-                            {userName && (
-                                <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>)}
-                            <NavDropdown.Divider />
-                            {!userName && (
-                                <NavDropdown.Item href="/register">Register</NavDropdown.Item>)}
-                            {!userName && (
-                                <NavDropdown.Item onClick={showLogin}>Login</NavDropdown.Item>)}
-                            {userName && (
-                                <NavDropdown.Item href="#" onClick={()=>setShowLogoutPopUp(true)}>Logout</NavDropdown.Item>)}
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/about">About</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        <LogoutPopUp
-            show={showLogoutPopUp}
+        <>
+            <Navbar bg="primary" variant="dark" expand="md">
+                <Container>
+                    <Navbar.Brand href="/home">Dorm Reviews</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <NavDropdown title="Dorms" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/dorms">Dorm Management</NavDropdown.Item>
+                                <NavDropdown.Item href="/reviews">Dorm reviews</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav className="ml-auto">
+                            <NavDropdown title={userName || "Login"} id="user-dropdown">
+                                {userName && (
+                                    <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>)}
+                                <NavDropdown.Divider />
+                                {!userName && (
+                                    <NavDropdown.Item href="/register">Register</NavDropdown.Item>)}
+                                {!userName && (
+                                    <NavDropdown.Item onClick={showLogin}>Login</NavDropdown.Item>)}
+                                {userName && (
+                                    <NavDropdown.Item href="#" onClick={() => setShowLogoutPopUp(true)}>Logout</NavDropdown.Item>)}
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="/about">About</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <LogoutPopUp
+                show={showLogoutPopUp}
                 handleClose={() => setShowLogoutPopUp(false)}
-                handleConfirm={() => logout() }
-        />
-    </>
+                handleConfirm={() => logout()}
+            />
+        </>
     );
 };
 
