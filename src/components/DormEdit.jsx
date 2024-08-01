cdb7703d9798d581a6f21f9164c64c4050d821cf
anpan7088 <anpan7088@gmail.com>
2024-08-01 20:13:13 +0200

    Some cleanup in DormEdit.jsx and explination comment  added

--- src/components/DormEdit.jsx
+++ src/components/DormEdit.jsx
@@ -1,8 +1,13 @@
+//src/components/DormEdit.jsx
 import { useState, useEffect } from 'react';
 import { Form, Button, Modal } from 'react-bootstrap';
 import CityAutocomplete from '../components/CityAutocomplete';
-import ReactJson from 'react-json-view';
 
+// Dorm edit form component, used to edit dorms data
+// if dorm is null, then the form is used to create a new dorm
+// if dorm is not null, then the form is used to edit the dorm
+// this commponent is used in DormsManagement.jsx
+// does not make call to the API directly, it uses the dorm data as a prop
 const DormEdit = ({ show, onHide, onSubmit, dorm }) => {
     const [formData, setFormData] = useState({
         name: '',
@@ -12,6 +17,8 @@ const DormEdit = ({ show, onHide, onSubmit, dorm }) => {
         lng: '',
     });
 
+    // useEffect hook to set the form data to the dorm data
+    // this is used to populate the form with the dorm data
     useEffect(() => {
         if (dorm) {
             setFormData({ name: dorm.name, address: dorm.address, city: dorm.city, lat: dorm.lat, lng: dorm.lng });
@@ -20,6 +27,8 @@ const DormEdit = ({ show, onHide, onSubmit, dorm }) => {
         }
     }, [dorm]);
 
+    // handle change
+    // this is a function that is called when the form field is changed
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({
@@ -28,6 +37,10 @@ const DormEdit = ({ show, onHide, onSubmit, dorm }) => {
         });
     };
 
+    // handle city change
+    // this is a custom component that is used to autocomplete the city field
+    // it is used in the DormEdit component and 
+    // defined in the components/CityAutocomplete.jsx
     const handleCityChange = (city) => {
         setFormData({
             ...formData,
@@ -56,6 +69,10 @@ const DormEdit = ({ show, onHide, onSubmit, dorm }) => {
         }
     };
 
+    // handle submit
+    // this is the function that is called when the form is submitted
+    // it is used in the DormsManagement.jsx
+    // onSubmit is a function that is passed as a prop from the DormsManagement.jsx
     const handleSubmit = (e) => {
         e.preventDefault();
         onSubmit(formData);
@@ -122,6 +139,15 @@ const DormEdit = ({ show, onHide, onSubmit, dorm }) => {
             </Form>
         </Modal>
     );
+};
+
+// define prop types
+import PropTypes from 'prop-types';
+DormEdit.propTypes = {
+    show: PropTypes.bool.isRequired,  // show prop is a boolean that determines if the modal is shown or not
+    onHide: PropTypes.func.isRequired, // onHide prop is a function that is called when the modal is closed
+    dorm: PropTypes.object,            // dorm prop is an object that contains the dorm data
+    onSubmit: PropTypes.func.isRequired, // onSubmit prop is a function that is called when the form is submitted
 };
 
 export default DormEdit;
